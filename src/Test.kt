1db3223118bc5d39f2548bdfba10fcc5dcca1aec
ondramisar <ondramisar124@seznam.cz>
2020-03-11 19:39:16 +0100

    Add buidling

--- src/Test.kt
+++ src/Test.kt
@@ -2,10 +2,12 @@ fun main() {
     val house = House(4)
     val newHouse = Department(2)
     val school = School()
+    val school3 = School()
 
     val buildings = arrayListOf<Building>()
     buildings.add(house)
     buildings.add(school)
+    buildings.add(school3)
 
     val city = City(buildings)
 
