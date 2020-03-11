91fa52d64b8a9aa2b424fcfacdcbd77e01312321
ondramisar <ondramisar124@seznam.cz>
2020-03-11 19:31:04 +0100

    Initial commit

---
+++ src/Test.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val house = House(4)
+    val school = School()
+
+    val buildings = arrayListOf<Building>()
+    buildings.add(house)
+    buildings.add(school)
+
+    val city = City(buildings)
+
+    println(city.returnNumberOfBuilding().toString())
+    println(city.returnNameOfFirst())
+    println(house.isNumberOfFlatNotZero().toString())
+}
